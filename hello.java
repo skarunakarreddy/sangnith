d3996bee6f59ad63e9b7084157a0cc47fdee9456
S Karunakar Reddy <43297546+skarunakarreddy@users.noreply.github.com>
2022-03-09 11:31:05 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class test{
-  public static void main(String a[]){
-    System.out("Hai guys..............");
-    System.out("Welcome to jenkins.....");
+  public static void main(String as[]){
+    System.out.println("Hai guys..............");
+    System.out.println("Welcome to jenkins.....");
   }
 }
