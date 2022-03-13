e17c9c5d3d6670a763975edbce6a85cc4b1de95b
S Karunakar Reddy <43297546+skarunakarreddy@users.noreply.github.com>
2022-03-13 11:15:47 +0530

    updated by reddy

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class hello{
   public static void main(String as[]){
     System.out.println("Hai guys..............");
     System.out.println("Welcome to jenkins.....");
+    System.out.println("updated by reddy .....");
   }
 }
